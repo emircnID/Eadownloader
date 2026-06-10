2e6cd3a08e8f5d9064a1967b5e998749bea8bbb8
emircnID <215845797+emircnID@users.noreply.github.com>
2026-06-10 13:55:27 +0300

    Polish localization copy

--- internal/localization/messages.go
+++ internal/localization/messages.go
@@ -5,190 +5,190 @@ import "github.com/nicksnyder/go-i18n/v2/i18n"
 var (
 	Language = &i18n.Message{
 		ID:    "Language",
-		Other: "english",
+		Other: "English",
 	}
 	StartMessage = &i18n.Message{
 		ID:    "StartMessage",
-		Other: "welcome {{.Name}}. send a supported social media link and I will fetch the media for you",
+		Other: "Welcome {{.Name}}. Send a supported social media link, and I will fetch the media for you.",
 	}
 	AddButton = &i18n.Message{
 		ID:    "AddButton",
-		Other: "add to a group",
+		Other: "Add to a Group",
 	}
 	ErrorMessage = &i18n.Message{
 		ID:    "ErrorMessage",
-		Other: "an error occurred, please try again later",
+		Other: "An error occurred. Please try again later.",
 	}
 	AddedToGroupMessage = &i18n.Message{
 		ID:    "AddedToGroupMessage",
-		Other: "thank you for adding me! use /settings command to configure the bot for this group",
+		Other: "Thank you for adding me! Use the /settings command to configure the bot for this group.",
 	}
 	SettingsButton = &i18n.Message{
 		ID:    "SettingsButton",
-		Other: "settings",
+		Other: "Settings",
 	}
 	LanguageButton = &i18n.Message{
 		ID:    "LanguageButton",
-		Other: "language",
+		Other: "Language",
 	}
 	PrivateSettingsMessage = &i18n.Message{
 		ID:    "PrivateSettingsMessage",
-		Other: "use the buttons below to change your personal bot settings",
+		Other: "Use the buttons below to change your personal bot settings.",
 	}
 	GroupSettingsMessage = &i18n.Message{
 		ID:    "GroupSettingsMessage",
-		Other: "use the buttons below to change this group's bot settings",
+		Other: "Use the buttons below to change this group's bot settings.",
 	}
 	BackButton = &i18n.Message{
 		ID:    "BackButton",
-		Other: "back",
+		Other: "Back",
 	}
 	SelectLanguageMessage = &i18n.Message{
 		ID:    "SelectLanguageMessage",
-		Other: "select your preferred language",
+		Other: "Select your preferred language.",
 	}
 	CaptionsSettingsMessage = &i18n.Message{
 		ID:    "CaptionsSettingsMessage",
-		Other: "when enabled, adds original description to downloaded content, if available",
+		Other: "When enabled, adds the original description to downloaded content if available.",
 	}
 	NsfwSettingsMessage = &i18n.Message{
 		ID:    "NsfwSettingsMessage",
-		Other: "when enabled, allows downloading nsfw content in this chat\n\nwarning: such content may violate telegram's terms of service and result in group restrictions",
+		Other: "When enabled, allows NSFW content to be downloaded in this chat.\n\nWarning: This type of content may violate Telegram's Terms of Service and result in group restrictions.",
 	}
 	SilentModeSettingsMessage = &i18n.Message{
 		ID:    "SilentModeSettingsMessage",
-		Other: "when enabled, the bot will not send error messages",
+		Other: "When enabled, the bot will not send error messages.",
 	}
 	MediaAlbumSettingsMessage = &i18n.Message{
 		ID:    "MediaAlbumSettingsMessage",
-		Other: "select maximum number of files allowed in a single media album",
+		Other: "Select the maximum number of files allowed in a single media album.",
 	}
 	InlineLoadingMessage = &i18n.Message{
 		ID:    "InlineLoadingMessage",
-		Other: "loading... please wait",
+		Other: "Loading... Please wait.",
 	}
 	InlineProcessingMessage = &i18n.Message{
 		ID:    "InlineProcessingMessage",
-		Other: "shared a media! processing download... please wait",
+		Other: "Media shared! Processing the download... Please wait.",
 	}
 	InlineShareMessage = &i18n.Message{
 		ID:    "InlineShareMessage",
-		Other: "share this media",
+		Other: "Share this media",
 	}
 	NoPermission = &i18n.Message{
 		ID:    "NoPermission",
-		Other: "you don't have permissions to perform this action",
+		Other: "You do not have permission to perform this action.",
 	}
 	CloseButton = &i18n.Message{
 		ID:    "CloseButton",
-		Other: "close",
+		Other: "Close",
 	}
 	MediaAlbumButton = &i18n.Message{
 		ID:    "MediaAlbumButton",
-		Other: "media album",
+		Other: "Media Album",
 	}
 	SilentModeButton = &i18n.Message{
 		ID:    "SilentModeButton",
-		Other: "silent mode",
+		Other: "Silent Mode",
 	}
 	CaptionsButton = &i18n.Message{
 		ID:    "CaptionsButton",
-		Other: "captions",
+		Other: "Captions",
 	}
 	NsfwButton = &i18n.Message{
 		ID:    "NsfwButton",
-		Other: "nsfw",
+		Other: "NSFW",
 	}
 	ExtractorsButton = &i18n.Message{
 		ID:    "ExtractorsButton",
-		Other: "extractors",
+		Other: "Platforms",
 	}
 	DisabledExtractorsSettingsMessage = &i18n.Message{
 		ID:    "DisabledExtractorsSettingsMessage",
-		Other: "select which extractors should be disabled. links from disabled extractors will be ignored by the bot",
+		Other: "Select which platforms should be disabled. Links from disabled platforms will be ignored by the bot.",
 	}
 	DeleteLinksButton = &i18n.Message{
 		ID:    "DeleteProcessedButton",
-		Other: "links",
+		Other: "Links",
 	}
 	DeleteLinksSettingsMessage = &i18n.Message{
 		ID:    "DeleteProcessedSettingsMessage",
-		Other: "when enabled, deletes the user's original message after successfully processing the link",
+		Other: "When enabled, deletes the user's original message after the link is processed successfully.",
 	}
 	SupportedExtractorsMessage = &i18n.Message{
 		ID:    "SupportedExtractorsMessage",
-		Other: "list of supported extractors by the bot",
+		Other: "Supported platforms",
 	}
 	EnabledButton = &i18n.Message{
 		ID:    "EnabledButton",
-		Other: "enabled",
+		Other: "Enabled",
 	}
 	DisabledButton = &i18n.Message{
 		ID:    "DisabledButton",
-		Other: "disabled",
+		Other: "Disabled",
 	}
 	ErrorUnavailable = &i18n.Message{
 		ID:    "ErrorUnavailable",
-		Other: "this content is unavailable",
+		Other: "This content is unavailable.",
 	}
 	ErrorTimeout = &i18n.Message{
 		ID:    "ErrorTimeout",
-		Other: "timeout error when downloading. try again later",
+		Other: "The download timed out. Please try again later.",
 	}
 	ErrorUnsupportedImageFormat = &i18n.Message{
 		ID:    "ErrorUnsupportedImageFormat",
-		Other: "unsupported image format",
+		Other: "Unsupported image format.",
 	}
 	ErrorUnsupportedExtractorType = &i18n.Message{
 		ID:    "ErrorUnsupportedExtractorType",
-		Other: "unsupported extractor type",
+		Other: "Unsupported platform type.",
 	}
 	ErrorMediaAlbumLimitExceeded = &i18n.Message{
 		ID:    "ErrorMediaAlbumLimitExceeded",
-		Other: "media album limit exceeds the maximum allowed for this group. change /settings to increase the limit",
+		Other: "The media album limit exceeds the maximum allowed for this group. Use /settings to increase the limit.",
 	}
 	ErrorMediaAlbumGlobalLimitExceeded = &i18n.Message{
 		ID:    "ErrorMediaAlbumGlobalLimitExceeded",
-		Other: "media album limit exceeds the maximum allowed for this instance",
+		Other: "The media album limit exceeds the maximum allowed for this instance.",
 	}
 	ErrorGeoRestrictedContent = &i18n.Message{
 		ID:    "ErrorGeoRestrictedContent",
-		Other: "this content has geo-restrictions and cannot be accessed from the server's location",
+		Other: "This content has geo-restrictions and cannot be accessed from the server's location.",
 	}
 	ErrorNSFWNotAllowed = &i18n.Message{
 		ID:    "ErrorNSFWNotAllowed",
-		Other: "this content is marked as nsfw and can't be downloaded in this group. change /settings to allow nsfw content or use the bot privately",
+		Other: "This content is marked as NSFW and cannot be downloaded in this group. Use /settings to allow NSFW content or use the bot privately.",
 	}
 	ErrorInlineMediaAlbum = &i18n.Message{
 		ID:    "ErrorInlineMediaAlbum",
-		Other: "you can't download media albums in inline mode. use the bot in a group or private chat",
+		Other: "Media albums cannot be downloaded in inline mode. Use the bot in a group or private chat.",
 	}
 	ErrorAuthenticationNeeded = &i18n.Message{
 		ID:    "ErrorAuthenticationNeeded",
-		Other: "this instance is not authenticated with this service",
+		Other: "This instance is not authenticated with this service.",
 	}
 	ErrorFileTooLarge = &i18n.Message{
 		ID:    "ErrorFileTooLarge",
-		Other: "this file is too large and exceeds the maximum allowed size for this instance",
+		Other: "This file is too large and exceeds the maximum allowed size for this instance.",
 	}
 	ErrorTelegramFileTooLarge = &i18n.Message{
 		ID:    "ErrorTelegramFileTooLarge",
-		Other: "this file is too large for telegram and exceeds the maximum allowed size",
+		Other: "This file is too large for Telegram.",
 	}
 	ErrorDurationTooLong = &i18n.Message{
 		ID:    "ErrorDurationTooLong",
-		Other: "this video is too long and exceeds the maximum allowed duration for this instance",
+		Other: "This video is too long and exceeds the maximum allowed duration for this instance.",
 	}
 	ErrorPaidContent = &i18n.Message{
 		ID:    "ErrorPaidContent",
-		Other: "this content is paid and requires a subscription to access",
+		Other: "This content is paid and requires a subscription to access.",
 	}
 	ErrorAgeRestricted = &i18n.Message{
 		ID:    "ErrorAgeRestricted",
-		Other: "this content is age-restricted and cannot be accessed",
+		Other: "This content is age-restricted and cannot be accessed.",
 	}
 	ErrorPermissionDenied = &i18n.Message{
 		ID:    "ErrorPermissionDenied",
-		Other: "the bot does not have sufficient permissions to send this media. please grant the necessary permissions and try again",
+		Other: "The bot does not have sufficient permissions to send this media. Please grant the necessary permissions and try again.",
 	}
 )
